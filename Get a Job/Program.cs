cc1620d3a5100cee8a2579e26fb8aed72e547b1f
taewook06 <109446725+taewook06@users.noreply.github.com>
2022-08-26 00:20:38 +0900

    123

--- Get a Job/Program.cs
+++ Get a Job/Program.cs
@@ -536,7 +536,7 @@ namespace Get_a_Job
                     Console.BackgroundColor = ConsoleColor.Red;
                     Clear();
                     ForegroundColor = ConsoleColor.Yellow;
-                    WriteLine("아쉽지만, 다음기회에...");
+                    WriteLine("아쉽지만, 다음기회에..");
                     Chance -= 1;
                     Thread.Sleep(2000);
                     StartPlayer();
